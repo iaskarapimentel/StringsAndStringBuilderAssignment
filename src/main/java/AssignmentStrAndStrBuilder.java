ba50d3f1837f5cb723153dcdab7c3da187f780c2
Iaskara Pimentel <iaiadivi@gmail.com>
2021-10-06 22:21:18 +0200

    updating methods

--- src/main/java/AssignmentStrAndStrBuilder.java
+++ src/main/java/AssignmentStrAndStrBuilder.java
@@ -1,21 +1,56 @@
 package main.java;
 
-import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class AssignmentStrAndStrBuilder {
 
     public static void main(String[] args) {
 
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Please enter here your text: ");
-        String userText = userInput.nextLine();
-        String checked = checkingNumbersOfChars(userText);
-        System.out.println(checked);
+        String userText, word1, word2, delWord;
+        char c1, c2;
 
-        System.out.println(replaceSpecChar(checked, "O", "A"));
+        Scanner scInput = new Scanner(System.in);
+        System.out.println("Please enter here your text: ");
+        userText = scInput.nextLine();
+//        check length :
+        System.out.println(checkingNumbersOfChars(userText));
+//        replacing specific character
+        System.out.println("First type the specific character to be replaced press enter and then type the replacement character: ");
+        c1 = scInput.next().charAt(0);
+        c2 = scInput.next().charAt(0);
+        userText = replaceSpecChar(userText, c1, c2);
+        System.out.println(userText);
+//        replacing all  equal characters
+        System.out.println("First type the character to be replaced press enter and then type the replacement character:");
+        c1 = scInput.next().charAt(0);
+        c2 = scInput.next().charAt(0);
+        userText = replaceAllChar(userText, c1, c2);
+        System.out.println(userText);
+//      replacing a specific word
+        System.out.println("First type the specific word to be replaced press enter and then type the replacement word:");
+        word1 = scInput.next();
+        word2 = scInput.next();
+        userText = replaceSpecPhrase(userText, word1, word2);
+        System.out.println(userText);
+//      replacing all equal words
+        System.out.println("First type the word to be replaced press enter and then type the replacement word:");
+        word1 = scInput.next();
+        word2 = scInput.next();
+        userText = replaceAllPhrase(userText, word1, word2);
+        System.out.println(userText);
+//      deleting a word
+        System.out.println("Enter the word you want to delete: ");
+        delWord = scInput.next();
+        userText = deleteWord(userText, delWord);
+        System.out.println(userText);
+
     }
 
+    /**
+     *
+     * @param text
+     * @return
+     */
     public static String checkingNumbersOfChars(String text){
         if(text.length() < 500) {
             return text;
@@ -23,26 +58,44 @@ public class AssignmentStrAndStrBuilder {
             return "Error your text must be less than 500 characters";
         }
     }
+    /**
+     *
+     * @param original
+     * @param oldChar
+     * @param newChar
+     * @return
+     */
+    public static String replaceSpecChar(String original, char oldChar, char newChar) {
 
-
-    public static StringBuilder replaceSpecChar(String original, String char1, String char2) {
-        StringBuilder str1 = new StringBuilder(original);
-        int startIndex = str1.indexOf(char1);
-        int endIndex = str1.indexOf(char2);
-        return str1.replace(startIndex, endIndex, original);
+        StringBuilder result = new StringBuilder(original);
+        int startIndex = original.indexOf(oldChar);
+        result.replace(startIndex, startIndex+1, String.valueOf(newChar));
+        return result.toString();
     }
 
-//    public static void replaceAllChar(String text) {
-//
-//    }
-//
-//    public static void replaceSpecPhrase(){
-//
-//    }
-//
-//    public  static void replaceAllPhrase() {
-//
-//    }
+    public static String replaceAllChar(String original, char oldChar, char newChar) {
+        return original.replace(oldChar, newChar);
+    }
 
+    public static String replaceSpecPhrase(String original, String oldWord, String newWord) {
 
+        StringBuilder result = new StringBuilder(original);
+        int startIndex = original.indexOf(oldWord);
+        int endIndex = startIndex + oldWord.length();
+        result.replace(startIndex, endIndex, newWord);
+        return result.toString();
+    }
+
+    public  static String replaceAllPhrase(String original, String oldWord, String newWord) {
+        return original.replace(oldWord, newWord);
+    }
+
+    public static String deleteWord(String original, String word1) {
+
+        StringBuilder result = new StringBuilder(original);
+        int startIndex = original.indexOf(word1);
+        int endIndex = startIndex + word1.length();
+        result.delete(startIndex, endIndex);
+        return result.toString();
+    }
 }
